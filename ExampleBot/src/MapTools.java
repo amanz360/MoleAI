eded81eb0468b219a5610155333db7f7b1f7f145
Mole <amanz360@gmail.com>
2016-12-08 05:02:45 -0500

    Added expanding and other bug fixes
    Functionality for expanding has been added. Issue with base defence
    breaking after attacking fixed. Changed medic behavior to force them to
    stay near marines, it's still not optimal.

---
+++ ExampleBot/src/MapTools.java
@@ -0,0 +1,67 @@
+import bwta.BWTA;
+import bwta.BaseLocation;
+import bwapi.*;
+
+public class MapTools 
+{
+	public static TilePosition getNextExpansion(Player player, Game game)
+	{
+	    BaseLocation closestBase = null;
+	    double minDistance = 100000;
+
+	    TilePosition homeTile = player.getStartLocation();
+
+	    // for each base location
+	    for (BaseLocation base : BWTA.getBaseLocations())
+	    {
+	    	// if the base has gas
+	        if (!base.isMineralOnly() && !(base == BWTA.getStartLocation(player)))
+	        {
+	            // get the tile position of the base
+	            TilePosition tile = base.getTilePosition();
+	            boolean buildingInTheWay = false;
+	           
+	            for (int x = 0; x < game.self().getRace().getCenter().tileWidth(); ++x)
+	            {
+	                for (int y = 0; y < game.self().getRace().getCenter().tileHeight(); ++y)
+	                {	                
+	                    TilePosition tp = new TilePosition(tile.getX() + x, tile.getY() + y);
+
+	                    for (Unit unit : game.getUnitsOnTile(tp))
+	                    {
+	                        if (unit.getType().isBuilding() && !unit.isFlying())
+	                        {
+	                            buildingInTheWay = true;
+	                            break;
+	                        }
+	                    }
+	                }
+	            }
+	            
+	            if (buildingInTheWay)
+	            {
+	                continue;
+	            }
+	            
+	            // the base's distance from our main nexus
+	            Position myBasePosition = player.getStartLocation().toPosition();
+	            Position thisTile = tile.toPosition();
+	            double distanceFromHome = myBasePosition.getDistance(thisTile);
+
+	            // if it is not connected, continue
+	            if (!BWTA.isConnected(homeTile,tile) || distanceFromHome < 0)
+	            {
+	                continue;
+	            }
+
+	            if (closestBase == null || distanceFromHome < minDistance)
+	            {
+	                closestBase = base;
+	                minDistance = distanceFromHome;
+	            }
+	        }
+	    }
+	    
+	    return closestBase.getTilePosition();
+	}
+}
